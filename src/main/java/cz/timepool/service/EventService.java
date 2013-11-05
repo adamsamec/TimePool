3007eb2bb81c8bd5a4e3d5852b6cfcceabf224c9
lukasinko <lukasinko@gmail.com>
2013-11-05 21:30:50 +0100

    lehke zmeny

---
+++ src/main/java/cz/timepool/service/EventService.java
@@ -0,0 +1,15 @@
+
+package cz.timepool.service;
+
+import cz.timepool.dto.EventDto;
+import java.util.List;
+
+/**
+ *
+ * @author Lukas Lowinger
+ */
+interface EventService {
+    //TODO: dopsat metody
+    public List<EventDto> getAllEvents();
+}
+
