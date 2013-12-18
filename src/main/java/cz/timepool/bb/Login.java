379f3849751bfcd1bdabf86b1d9ee9d2e48cda0c
Adam Samec <adam.samec@gmail.com>
2013-12-18 11:13:50 +0100

    Úpravy login a registration formuláře - Přidání Go back do registration formuláře - Přidání hlášky o úspěšné registraci do login formuláře

--- src/main/java/cz/timepool/bb/Login.java
+++ src/main/java/cz/timepool/bb/Login.java
@@ -15,6 +15,11 @@ public class Login {
     public boolean isAuthFailure() {
         String authFailureParam = FacesUtil.getRequestParameter("auth_failure");
         return ((authFailureParam != null) && authFailureParam.equals("true"));
+    }   
+
+    public boolean isRegistrationSuccess() {
+        String registrationSuccess = FacesUtil.getRequestParameter("registration_success");
+        return ((registrationSuccess != null) && registrationSuccess.equals("true"));
     }
 
 }
