8d72a8e947a2b226443575132742c53dba8bcf09
Adam Samec <adam.samec@gmail.com>
2013-12-28 02:43:27 +0100

    Přidání povinnosti zadat heslo při registraci + odstranění zbytečných metod z LoginSession.java

--- TimePool/src/main/java/cz/timepool/pres/bb/LoginSession.java
+++ TimePool/src/main/java/cz/timepool/pres/bb/LoginSession.java
@@ -4,11 +4,8 @@ import cz.timepool.bo.UserRole;
 import cz.timepool.dto.UserDto;
 import cz.timepool.service.UsersServiceIface;
 import java.io.Serializable;
-import java.util.Collection;
 import javax.faces.bean.SessionScoped;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -23,24 +20,13 @@ public class LoginSession implements Serializable {
     @Autowired
     private UsersServiceIface usersService;
 
-    private UserDto user;
-
-//     private boolean isAdmin;
-
     public String getUserEmail() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
-    // TODO: Pri prvnim volani ulozit do this.user a zaroven nastavit this.isAdmin
-    // TODO: ma smysl to cachovat do this.user nebo to zajisti entityManager?
+    // TODO: Cachovani zajisti entityManager?
     public UserDto getUser() {
-        setUser(usersService.getUserByEmail(getUserEmail()));
-        return user;
-    }
-
-    // TODO: proc je to umozneno?
-    public void setUser(UserDto user) {
-        this.user = user;
+        return usersService.getUserByEmail(getUserEmail());
     }
 
     public boolean isIsAdmin() {
